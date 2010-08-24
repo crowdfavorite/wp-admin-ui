71445c27852d28c8804f40c95523485208140b19
ejdanderson@gmail.com <ejdanderson@gmail.com@34d9dc3e-6279-e759-70c0-36221e8e1617>
2010-08-24 22:42:32 +0000

    Updating cf-banner
    git-svn-id: http://cf-wpplugin-admin-ui.googlecode.com/svn/trunk@6 34d9dc3e-6279-e759-70c0-36221e8e1617

--- includes/cf-banner.php
+++ includes/cf-banner.php
@@ -2,6 +2,6 @@
 	<div class="wphc-content">
 		<p><strong>Support available from <a href="http://wphelpcenter.com/" title="WordPress HelpCenter">WordPress HelpCenter</a></strong> <a href="#">Submit a support request</a> or call 303.395.1346</p>
 		<p><a href="" id="wphc-close">close</a></p>
-		<p><a href="" class="wphc-support-btn">Support</a></p>
 	</div><!-- .cf-box-content -->
 </div><!-- #cf-support-widget -->
+<p><a href="" class="wphc-support-btn">Support</a></p>
