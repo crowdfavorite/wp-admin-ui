5d2d0f62f5d378db24a199794501755576358797
ejdanderson@gmail.com <ejdanderson@gmail.com@34d9dc3e-6279-e759-70c0-36221e8e1617>
2010-08-30 21:54:42 +0000

    Fixed settings form display error
    git-svn-id: http://cf-wpplugin-admin-ui.googlecode.com/svn/trunk@15 34d9dc3e-6279-e759-70c0-36221e8e1617

--- includes/cf-settings-form.php
+++ includes/cf-settings-form.php
@@ -2,7 +2,6 @@
 	print('
 		<form id="' . $plugin_slug . '_settings_form" name="' . $plugin_slug . '_settings_form" action="'.get_bloginfo('wpurl').'/wp-admin/options-general.php" method="post" class="elm-width-300">
 			<input type="hidden" name="cf_action" value="'.$plugin_slug . '_update_settings" />
-			<p>'.__('Browsers that have a <a href="http://en.wikipedia.org/wiki/User_agent">User Agent</a> matching a key below will be shown the mobile version of your site instead of the normal theme.', $text_domain).'</p>
 			<fieldset class="lbl-pos-left">
 	');
 	foreach ($settings as $key => $config) {
